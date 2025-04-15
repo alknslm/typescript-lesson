715f9627cbe3e0ca1ee19d2f9313a678eb65a65c
alknslm <slmalkn.97@gmail.com>
2025-04-15 23:12:35 +0300

    interface-type-function

--- 1.Typescript-Baslangic/src/App.tsx
+++ 1.Typescript-Baslangic/src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Interface ve type kullanımı 
+  //? Optional Type: ? işareti ile tanımlanır. Bu işaret, bir özelliğin isteğe bağlı olduğunu belirtir.
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  //! Type kullanımı 
+  type User = {
+    name: string,
+    age: number
+  }
+
+  const object1: User = {
+    name: "Ali",
+    age: 25
+  }
+
+  let array: User[] = [];
+
+  //! Interface kullanımı
+  interface User2 {
+    name: string,
+    age?: number // age olsada olur olmasada olur
+  }
+
+  //* nesneyi oluştururken interfacein içindeki özellikleri kullanmak zorundayız.
+  const object2: User2 = {
+    name: "Ahmet",
+    age: 38
+  }
+
+  const object3: User2 = {
+    name: "Veysel"
+  }
+
+  console.log(object1)
+  console.log(object2);
+  console.log(object3);
+
+
+  //! FUNCTIONLAR
+
+  function topla(a: number, b: number) {
+    //* numberdan başka bir tür verilirse hata verir. number yerine kendi interfaceimizi de yazabiliriz.
+    console.log(a + b)
+  }
+
+  function topla2(a: number, b: number): number | string { //* topla2 fonksiyonu number yada string döndürür.    
+    //return (a + b)
+    return "Toplama işlemi başarılı";
+  }
+
+  topla(5, 10);
+  console.log(topla2(5, 10)); // 15
+
+  //? geriye bir şey döndürmeyecekse void yazılır.
+  function write(array: Array<string>): void { // arrayin içindeki elemanlar string olmalı.
+    array.forEach((value) => console.log(value));
+  }
+
+  write(["Ali", "Veysel", "Ahmet"]);
+
+
+  //!-------------------------------------------
+
+  interface User3 {
+    name: string,
+    age: number
+  }
+
+  function writeConsole(array: Array<User3>): void { // arrayin içindeki elemanlar User3 olmalı.
+    array.forEach((value: User3) => console.log(value.name, value.age));
+  }
+
+  const obj1: User3 = {
+    name: "Ali",
+    age: 25
+  }
+
+  const obj2: User3 = {
+    name: "Ahmet",
+    age: 30
+  }
+
+  let myArray: Array<User3> = [obj1, obj2];
+
+  writeConsole(myArray);
+
+  return (<div></div>)
 }
 
 export default App
